b6285deb340823149f328a1f8733286265c1bddd
Ariel Wolle <36164850+ArielWolle@users.noreply.github.com>
2021-04-01 16:28:41 -0400

    commented code

--- Code/code.py
+++ Code/code.py
@@ -6,8 +6,6 @@ import pwmio
 from adafruit_motor import servo
 from adafruit_motor import stepper
 
-DELAY = 0.01
-STEPS = 200
 
 # Setup Joystick pins
 x_plus = digitalio.DigitalInOut(board.GP0)
@@ -69,29 +67,52 @@ y_motor = stepper.StepperMotor(
 )
 y_motor.release()
 
+# Setup endstop pins
+x_plus_endstop = digitalio.DigitalInOut(board.GP14)
+x_minus_endstop = digitalio.DigitalInOut(board.GP15)
+y_plus_endstop = digitalio.DigitalInOut(board.GP16)
+y_minus_endstop = digitalio.DigitalInOut(board.GP17)
+
+# Setup endstop pins as input
+x_plus_endstop.direction = digitalio.Direction.INPUT
+x_minus_endstop.direction = digitalio.Direction.INPUT
+y_plus_endstop.direction = digitalio.Direction.INPUT
+y_minus_endstop.direction = digitalio.Direction.INPUT
+
 # Setup movement variables
 angle = 0
 my_servo.angle = 0
 time.sleep(1)
 
+# Main Loops
 
 while True:
-    if x_plus.value:
+
+    # Check for x movement in joystick and that the pen plotter is not at max/min x
+    if x_plus.value and not (x_plus_endstop.value):
         x_motor.onestep(direction=stepper.FORWARD)
-    elif x_minus:
+    elif x_minus and not (x_minus_endstop.value):
         x_motor.onestep(direction=stepper.BACKWARD)
 
-    if y_plus.value:
+    # Check for y movement in joystick and that the pen plotter is not at max/min y
+    if y_plus.value and not (y_plus_endstop.value):
         y_motor.onestep(direction=stepper.FORWARD)
-    elif y_minus.value:
+    elif y_minus.value and not (y_minus_endstop.value):
         y_motor.onestep(direction=stepper.BACKWARD)
 
-    if z_up.value:
-        if angle + 5 < 180:
-            angle += 5
-            my_servo.angle = angle
-    elif z_down.value:
-        if angle - 5 > 0:
-            angle -= 5
-            my_servo.angle = angle
+    # Checks that only on button is pressed for z movement
+    if not (z_up.value and z_down.value):
+        # Checks if z value is not at max and up button is pressed
+        if z_up.value:
+            if angle + 2 < 180:
+                angle += 5
+                my_servo.angle = angle
+
+        # Checks if z value is not at min and down button is pressed
+        elif z_down.value:
+            if angle - 2 > 0:
+                angle -= 5
+                my_servo.angle = angle
+
+    # Wait 0.01 seconds this might be useless so it is temporary
     time.sleep(0.01)
